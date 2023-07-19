74666cf01d987d0239a0487551a11263c945845e
Edward <kristiololo12@gmail.com>
2023-07-19 09:35:38 +0300

    fix

--- check_status.sh
+++ check_status.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-log_output=$(docker logs einfahrt -n 10 | grep "Обновление статусов доступности ЦОД")
+log_output=$(docker logs einfahrt -n 20 | grep -m 1 "Обновление статусов доступности ЦОД")
 
 # Извлечение значения для NODE1
-node1_status=$(echo "$log_output" | grep -o 'NODE1=Availability[^}]*' | awk -F', ' '{print $2}' | awk -F'=' '{print $2}')
+node1_status=$(echo "$log_output" | awk -F'NODE1=Availability' '{print $2}' | awk -F', ' '{print $2}' | awk -F'=' '{print $2}')
 
 # Извлечение значения для NODE2
-node2_status=$(echo "$log_output" | grep -o 'NODE2=Availability[^}]*' | awk -F', ' '{print $2}' | awk -F'=' '{print $2}')
+node2_status=$(echo "$log_output" | awk -F'NODE2=Availability' '{print $2}' | awk -F', ' '{print $2}' | awk -F'=' '{print $2}')
 
 echo "NODE1=$node1_status"
 echo "NODE2=$node2_status"
